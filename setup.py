86ae887eb705ea7142ff03ea5bcee8f250e9908c
Sahan Bulathwela <sahabull@gmail.com>
2019-03-26 19:20:24 +0000

    feat: added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='x5learn',
+    version='0.1',
+    packages=['server', 'test-integration', 'test-integration.server', 'test-integration.server.db'],
+    url='x5learn.org',
+    license='',
+    author='x5gon',
+    author_email='',
+    description=''
+)

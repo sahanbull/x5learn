165b68017511c0c8fe2158188fb0d678a74d96c5
Sahan Bulathwela <sahabull@gmail.com>
2019-04-12 18:05:45 +0100

    feat: final ll

--- x5learn_server/app.py
+++ x5learn_server/app.py
@@ -45,11 +45,9 @@ user_datastore = SQLAlchemySessionUserDatastore(db_session,
 security = Security(app, user_datastore)
 mail.init_app(app)
 
-
 # Initial OER data from CSV files
 loaded_oers = {}
 all_entity_titles = set([])
-
 
 
 # create database when starting the app
@@ -64,13 +62,16 @@ def initiate_login_db():
 def home():
     return render_template('home.html')
 
+
 @app.route("/login")
 def login():
     return render_template('security/login_user.html')
 
+
 @app.route("/signup")
 def signup():
     return render_template('security/register_user.html')
+
 
 @app.route("/logout")
 # @login_required
@@ -79,9 +80,11 @@ def logout():
     logout_user()
     return redirect("/")
 
+
 @app.route("/search")
 def search():
     return render_template('home.html')
+
 
 # @app.route("/next_steps")
 # def next_steps():
@@ -99,9 +102,11 @@ def search():
 def bookmarks():
     return render_template('home.html')
 
+
 @app.route("/history")
 def history():
     return render_template('home.html')
+
 
 # @app.route("/notes")
 # def notes():
@@ -116,7 +121,7 @@ def profile():
 @app.route("/api/v1/session/", methods=['GET'])
 def api_session():
     if current_user.is_authenticated:
-        user_profile = { 'email': current_user.email, 'firstName': 'Glen', 'lastName': 'Morangie' }
+        user_profile = {'email': current_user.email, 'firstName': 'Glen', 'lastName': 'Morangie'}
         # TODO: get the email, first name and last name from the db
         # if record doesn't exist, set firstName and lastName to empty strings
         # use current_user.get_id()
@@ -125,12 +130,13 @@ def api_session():
     else:
         pseudonym = request.cookies.get('x5learn_guest_pseudonym')
         print('pseudonym in cookie:', pseudonym)
-        if pseudonym==None or pseudonym=='':
-            pseudonym = 'Anonymous_user_'+str(randint(1,1000000))
+        if pseudonym == None or pseudonym == '':
+            pseudonym = 'Anonymous_user_' + str(randint(1, 1000000))
         print('pseudonym:', pseudonym)
         resp = jsonify({'guest': 'Anonymous'})
         resp.set_cookie('x5learn_guest_pseudonym', pseudonym)
         return resp
+
 
 @app.route("/api/v1/search/", methods=['GET'])
 def api_search():
@@ -165,7 +171,8 @@ def api_entity_descriptions():
     entity_ids = request.args['ids'].split(',')
     descriptions = {}
     conn = http.client.HTTPSConnection("www.wikidata.org")
-    request_string = '/w/api.php?action=wbgetentities&props=descriptions&ids=' + '|'.join(entity_ids) + '&languages=en&sitefilter=enwiki&languagefallback=1&format=json'
+    request_string = '/w/api.php?action=wbgetentities&props=descriptions&ids=' + '|'.join(
+        entity_ids) + '&languages=en&sitefilter=enwiki&languagefallback=1&format=json'
     conn.request('GET', request_string)
     response = conn.getresponse().read().decode("utf-8")
     j = json.loads(response)
@@ -198,7 +205,7 @@ def api_save_user_profile():
 
 def load_initial_dataset_from_csv():
     global loaded_oers
-    if len(loaded_oers)==0:
+    if len(loaded_oers) == 0:
         read_local_oer_data()
         setup_dummy_user()
 
@@ -215,7 +222,8 @@ class DummyUser:
     def viewed_fragments(self):
         if not self.fragments:
             print('creating viewed fragments')
-            self.fragments = [ create_fragment('Lecture 01 - The Learning Problem', 0, 1), create_fragment('Lecture 02 - Is Learning Feasible?', 0, 0.33) ]
+            self.fragments = [create_fragment('Lecture 01 - The Learning Problem', 0, 1),
+                              create_fragment('Lecture 02 - Is Learning Feasible?', 0, 0.33)]
             # self.fragments = [ create_fragment('Lecture 02 - Is Learning Feasible?', 0, 0.33) ]
         return self.fragments
 
@@ -235,13 +243,15 @@ class DummyUser:
         return result
 
     def recommended_next_steps(self):
-        return [ create_pathway("Continue studying", [ create_fragment('Lecture 02 - Is Learning Feasible?', 0.33, 1-0.33) ]),
-            create_pathway("20-minute sprint", [ create_fragment("S18.3 Hoeffding's Inequality", 0, 1) ]),
-            create_pathway("10-minute sprint", [ create_fragment('Lecture 02 - Is Learning Feasible?', 0.33, 10/76) ])
-            ]
+        return [create_pathway("Continue studying",
+                               [create_fragment('Lecture 02 - Is Learning Feasible?', 0.33, 1 - 0.33)]),
+                create_pathway("20-minute sprint", [create_fragment("S18.3 Hoeffding's Inequality", 0, 1)]),
+                create_pathway("10-minute sprint",
+                               [create_fragment('Lecture 02 - Is Learning Feasible?', 0.33, 10 / 76)])
+                ]
 
 
-def  search_results_from_experimental_local_oer_data(text):
+def search_results_from_experimental_local_oer_data(text):
     max_results = 18
     frequencies = defaultdict(int)
     for video_id, oer in loaded_oers.items():
@@ -250,7 +260,8 @@ def  search_results_from_experimental_local_oer_data(text):
                 if text == entity['title'].lower().strip():
                     frequencies[video_id] += 1
     # import pdb; pdb.set_trace()
-    results = [ loaded_oers[video_id] for video_id,freq in sorted(frequencies.items(), key=lambda k_v : k_v[1], reverse=True)[:max_results] ]
+    results = [loaded_oers[video_id] for video_id, freq in
+               sorted(frequencies.items(), key=lambda k_v: k_v[1], reverse=True)[:max_results]]
     print(len(results), 'search results found based on wikichunks.')
     # if len(results) < max_results:
     #     search_words = text.split()
@@ -292,14 +303,15 @@ def load_oers_from_csv_file():
         for oer in csv.DictReader(f, delimiter='\t'):
             url = oer['url']
             if url in loaded_oers:
-                continue # omit duplicates
+                continue  # omit duplicates
             if not oer['title']:
-                continue # omit incomplete items
+                continue  # omit incomplete items
             oer['images'] = json.loads(oer['images'].replace("'", '"'))
             oer['date'] = oer['date'].replace('Published on ', '') if 'date' in oer else ''
             oer['duration'] = human_readable_time_from_ms(float(oer['duration'])) if 'duration' in oer else ''
-            del oer['wikichunks'] # Use the new wikifier results instead (from the JSON files).
-            del oer['transcript'] # Delete in order to prevent unnecessary network load when serving OER to the frontend.
+            del oer['wikichunks']  # Use the new wikifier results instead (from the JSON files).
+            del oer[
+                'transcript']  # Delete in order to prevent unnecessary network load when serving OER to the frontend.
             videoid = oer['url'].split('v=')[1].split('&')[0]
             loaded_oers[videoid] = oer
     print('Done loading oers')
@@ -311,7 +323,7 @@ def load_wikichunks_from_json_files():
     dir_path = '/Users/stefan/x5/data/scenario1/youtube_enrichments/'
     (_, _, filenames) = next(os.walk(dir_path))
     for filename in filenames:
-        with open(dir_path+filename, newline='') as f:
+        with open(dir_path + filename, newline='') as f:
             for line in f:
                 chunk = json.loads(line)
                 oer = loaded_oers[chunk['videoid']]
@@ -322,7 +334,7 @@ def load_wikichunks_from_json_files():
     print('Done loading wikichunks')
     print('______________')
     print('Encoding wikichunks')
-    for videoid,oer in loaded_oers.items():
+    for videoid, oer in loaded_oers.items():
         url = oer['url']
         if not url in chunkdata:
             # print('WARNING: oer has no JSON chunks', videoid)
@@ -334,7 +346,7 @@ def load_wikichunks_from_json_files():
             duration = last_chunk['start'] + last_chunk['length']
             for j in json_chunks:
                 annotations = j['annotations']['annotation_data']
-                annotations = annotations[:7] # use the top ones, assuming they come sorted by pagerank
+                annotations = annotations[:7]  # use the top ones, assuming they come sorted by pagerank
                 annotations = sorted(annotations, key=lambda a: a['cosine'], reverse=True)
                 entities = []
                 for a in annotations:
@@ -352,13 +364,13 @@ def load_wikichunks_from_json_files():
 def encode_chunk(start_second, length_seconds, entities, duration):
     start = round(start_second / duration, 4)
     length = round(length_seconds / duration, 4)
-    return {'start': start, 'length': length, 'entities': entities }
+    return {'start': start, 'length': length, 'entities': entities}
 
 
 def human_readable_time_from_ms(ms):
     minutes = int(ms / 60000)
-    seconds = int(ms/1000 - minutes*60)
-    return str(minutes)+':'+str(seconds).rjust(2, '0')
+    seconds = int(ms / 1000 - minutes * 60)
+    return str(minutes) + ':' + str(seconds).rjust(2, '0')
 
 
 def create_fragment(oer_title, start, length):
@@ -372,16 +384,16 @@ def create_pathway(rationale, fragments):
 
 def find_oer_by_title(title):
     try:
-        return [ oer for oer in loaded_oers.values() if oer['title']==title ][0]
+        return [oer for oer in loaded_oers.values() if oer['title'] == title][0]
     except IndexError:
         print('No OER was found with title', title)
 
 
 def search_suggestions(text):
-    matches = [ (title, fuzz.partial_ratio(text, title) + fuzz.ratio(text, title)) for title in all_entity_titles ]
+    matches = [(title, fuzz.partial_ratio(text, title) + fuzz.ratio(text, title)) for title in all_entity_titles]
     matches = sorted(matches, key=lambda k_v: k_v[1], reverse=True)[:20]
-    print([ v for k,v in matches ])
-    matches = [ k for k,v in matches ]
+    print([v for k, v in matches])
+    matches = [k for k, v in matches]
     # import pdb; pdb.set_trace()
     print(matches)
     return jsonify(matches)

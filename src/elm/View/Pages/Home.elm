23e846d3f846be169be51cf4b2c3214cef2b2860
Stefan Kreitmayer <stefankreitmayer@gmail.com>
2019-04-15 14:35:14 +0100

    add exclamation mark

--- src/elm/View/Pages/Home.elm
+++ src/elm/View/Pages/Home.elm
@@ -22,7 +22,7 @@ viewHomePage model =
   let
       title =
         -- "Easy entrance to deep ideas." |> text |> el [ whiteText, Font.size 48, centerX, centerY ] |> el [ width fill, height <| fillPortion 3 ]
-        [ "Study anything. Anywhere. Free" |> text |> el [ whiteText, Font.size 60, centerX ]
+        [ "Study anything. Anywhere. Free!" |> text |> el [ whiteText, Font.size 60, centerX ]
         , "Easy entrance to deep ideas" |> text |> el [ whiteText, Font.size 24, centerX ]
         ]
         |> column [ centerX, centerY, spacing 30 ]

435cb4efd29906d79909c9c091044d5ae11288ed
sahanbull <sahanbull@gmail.com>
2019-04-15 13:05:03 +0000

    move some build tasks into init.sh

--- init.sh
+++ init.sh
@@ -3,3 +3,9 @@
 PYTHON_PATH="/home/ucl/anaconda3/envs/x5learn/bin/"
 
 $PYTHON_PATH/pip install -e .
+
+npm run setup
+npm run build
+
+cp -r uncompressed x5learn_server/static/dist
+cp -r assets/img/* x5learn_server/static/dist/img

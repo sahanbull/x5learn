9dc59a368858e7cab53cd2317757ce1e124ef6d7
Stefan Kreitmayer <stefankreitmayer@gmail.com>
2019-06-23 12:21:38 +0100

    ingest youtube videos via script parameter

--- ingest_scripts/youtube.py
+++ ingest_scripts/youtube.py
@@ -2,6 +2,7 @@ import sys
 import os
 import json
 import jsondiff
+import argparse
 
 from datetime import datetime
 
@@ -182,71 +183,6 @@ def scrape_youtube_page(videoid):
 ###########################################################
 
 
-CALTECH = ['https://www.youtube.com/watch?v=mbyG85GZ0PI&list=PLD63A284B7615313A&index=1',
-     'https://www.youtube.com/watch?v=MEG35RDD7RA&list=PLD63A284B7615313A&index=2',
-     'https://www.youtube.com/watch?v=FIbVs5GbBlQ&list=PLD63A284B7615313A&index=3',
-     'https://www.youtube.com/watch?v=L_0efNkdGMc&list=PLD63A284B7615313A&index=4',
-     'https://www.youtube.com/watch?v=SEYAnnLazMU&list=PLD63A284B7615313A&index=5',
-     'https://www.youtube.com/watch?v=6FWRijsmLtE&list=PLD63A284B7615313A&index=6',
-     'https://www.youtube.com/watch?v=Dc0sr0kdBVI&list=PLD63A284B7615313A&index=7',
-     'https://www.youtube.com/watch?v=zrEyxfl2-a8&list=PLD63A284B7615313A&index=8',
-     'https://www.youtube.com/watch?v=qSTHZvN8hzs&list=PLD63A284B7615313A&index=9',
-     'https://www.youtube.com/watch?v=Ih5Mr93E-2c&list=PLD63A284B7615313A&index=10',
-     'https://www.youtube.com/watch?v=EQWr3GGCdzw&list=PLD63A284B7615313A&index=11',
-     'https://www.youtube.com/watch?v=I-VfYXzC5ro&list=PLD63A284B7615313A&index=12',
-     'https://www.youtube.com/watch?v=o7zzaKd0Lkk&list=PLD63A284B7615313A&index=13',
-     'https://www.youtube.com/watch?v=eHsErlPJWUU&list=PLD63A284B7615313A&index=14',
-     'https://www.youtube.com/watch?v=XUj5JbQihlU&list=PLD63A284B7615313A&index=15',
-     'https://www.youtube.com/watch?v=O8CfrnOPtLc&list=PLD63A284B7615313A&index=16',
-     'https://www.youtube.com/watch?v=EZBUDG12Nr0&list=PLD63A284B7615313A&index=17',
-     'https://www.youtube.com/watch?v=ihLwJPHkMRY&list=PLD63A284B7615313A&index=18']
-
-STANFORD = ['https://www.youtube.com/watch?v=6QRpDLj8huE&list=PLoR5VjrKytrCv-Vxnhp5UyS1UjZsXP0Kj&index=1',
-     'https://www.youtube.com/watch?v=W46UTQ_JDPk&list=PLoR5VjrKytrCv-Vxnhp5UyS1UjZsXP0Kj&index=2',
-     'https://www.youtube.com/watch?v=WXNUbLC8A4I&list=PLoR5VjrKytrCv-Vxnhp5UyS1UjZsXP0Kj&index=3',
-     'https://www.youtube.com/watch?v=UVCFaaEBnTE&list=PLoR5VjrKytrCv-Vxnhp5UyS1UjZsXP0Kj&index=4',
-     'https://www.youtube.com/watch?v=lZit4Uzlswc&list=PLoR5VjrKytrCv-Vxnhp5UyS1UjZsXP0Kj&index=5',
-     'https://www.youtube.com/watch?v=4u81xU7BIOc&list=PLoR5VjrKytrCv-Vxnhp5UyS1UjZsXP0Kj&index=6',
-     'https://www.youtube.com/watch?v=QjOILAQ0EFg&list=PLoR5VjrKytrCv-Vxnhp5UyS1UjZsXP0Kj&index=7',
-     'https://www.youtube.com/watch?v=SGEroEKFbnY&list=PLoR5VjrKytrCv-Vxnhp5UyS1UjZsXP0Kj&index=8',
-     'https://www.youtube.com/watch?v=UVjj2fHu9YU&list=PLoR5VjrKytrCv-Vxnhp5UyS1UjZsXP0Kj&index=9',
-     'https://www.youtube.com/watch?v=zNhCF97exlA&list=PLoR5VjrKytrCv-Vxnhp5UyS1UjZsXP0Kj&index=10',
-     'https://www.youtube.com/watch?v=Tppi2Fof1DE&list=PLoR5VjrKytrCv-Vxnhp5UyS1UjZsXP0Kj&index=11',
-     'https://www.youtube.com/watch?v=uV5TnFc7eaE&list=PLoR5VjrKytrCv-Vxnhp5UyS1UjZsXP0Kj&index=12',
-     'https://www.youtube.com/watch?v=0D4LnsJr85Y&list=PLoR5VjrKytrCv-Vxnhp5UyS1UjZsXP0Kj&index=13',
-     'https://www.youtube.com/watch?v=pAwjiGkafbM&list=PLoR5VjrKytrCv-Vxnhp5UyS1UjZsXP0Kj&index=14',
-     'https://www.youtube.com/watch?v=UqqPm-Q4aMo&list=PLoR5VjrKytrCv-Vxnhp5UyS1UjZsXP0Kj&index=15',
-     'https://www.youtube.com/watch?v=GIcuSNAAa4g&list=PLoR5VjrKytrCv-Vxnhp5UyS1UjZsXP0Kj&index=16',
-     'https://www.youtube.com/watch?v=ed4whd9B-xw&list=PLoR5VjrKytrCv-Vxnhp5UyS1UjZsXP0Kj&index=17',
-     'https://www.youtube.com/watch?v=_GvMC0ZYvK8&list=PLoR5VjrKytrCv-Vxnhp5UyS1UjZsXP0Kj&index=18',
-     'https://www.youtube.com/watch?v=oByDE-RJkZA&list=PLoR5VjrKytrCv-Vxnhp5UyS1UjZsXP0Kj&index=19']
-
-
-# https://www.youtube.com/results?search_query=machine+learning+introduction
-FIRST_PAGE_OF_SEARCH_RESULTS_FOR_MACHINE_LEARNING_INTRODUCTION = ['https://www.youtube.com/watch?v=ukzFI9rgwfU',
-     'https://www.youtube.com/watch?v=IpGxLWOIZy4',
-     'https://www.youtube.com/watch?v=h0e2HAPTGF4',
-     'https://www.youtube.com/watch?v=ujTCoH21GlA',
-     'https://www.youtube.com/watch?v=hjh1ikznScg',
-     'https://www.youtube.com/watch?v=Gv9_4yMHFhI',
-     'https://www.youtube.com/watch?v=GvQwE2OhL8I',
-     'https://www.youtube.com/watch?v=JgvyzIkgxF0',
-     'https://www.youtube.com/watch?v=4vGiHC35j9s',
-     'https://www.youtube.com/watch?v=nKW8Ndu7Mjw',
-     'https://www.youtube.com/watch?v=cKxRvEZd3Mw',
-     'https://www.youtube.com/watch?v=JN6H4rQvwgY',
-     'https://www.youtube.com/watch?v=63NTeLmDANo',
-     'https://www.youtube.com/watch?v=8onB7rPG4Pk',
-     'https://www.youtube.com/watch?v=SN2BZswEWUA',
-     'https://www.youtube.com/watch?v=Q59X518JZHE',
-     'https://www.youtube.com/watch?v=aircAruvnKk',
-     'https://www.youtube.com/watch?v=5hNK7-N23eU',
-     # 'https://www.youtube.com/watch?v=-DEL6SVRPw0', Error: No transcript available
-     'https://www.youtube.com/watch?v=BR9h47Jtqyw',
-     'https://www.youtube.com/watch?v=O5xeyoRL95U',
-     'https://www.youtube.com/watch?v=J1_A-rdNBNQ']
-
-
 def human_readable_time_from_seconds(seconds):
     minutes = int(seconds / 60)
     seconds = int(seconds - minutes * 60)
@@ -254,7 +190,6 @@ def human_readable_time_from_seconds(seconds):
 
 
 def ingest_oer_from_url(url):
-    # import pdb; pdb.set_trace()
     print('\n______________________________________________________________')
     print(url)
     oer = session.query(Oer).filter_by(url=url).first()
@@ -262,7 +197,6 @@ def ingest_oer_from_url(url):
         print('Exists already -> skipping.')
         return True
     videoid = url.split('watch?v=')[1].split('&')[0]
-    print(videoid)
     data = scrape_youtube_page(videoid)
     if isinstance(data, str):
         print('Error:', data)
@@ -286,7 +220,12 @@ def ingest_oer_from_url(url):
 
 
 if __name__ == '__main__':
-    urls = CALTECH + STANFORD + FIRST_PAGE_OF_SEARCH_RESULTS_FOR_MACHINE_LEARNING_INTRODUCTION
+    parser=argparse.ArgumentParser(
+        description='''X5Learn ingestion script for youtube videos. ''',
+        epilog="""NB existing videos won't be overwritten.""")
+    parser.add_argument('urls', type=str, help='Text file containing a newline-separated list of video urls')
+    args=parser.parse_args()
+    urls = open(args.urls).readlines()
 
     for url in urls:
         success = ingest_oer_from_url(url)

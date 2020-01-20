eab9c8fd245debae8ce261d26c81777a0c5942a1
FurNasH <shenrocks@live.com>
2020-01-20 10:51:26 +0530

    added comments to where x5gon cookie should get init

--- src/js/cookiehub.js
+++ src/js/cookiehub.js
@@ -3,45 +3,24 @@ window.addEventListener("load", function() {
       onInitialise: function(status) 
       {
         if (this.hasConsented('required')) {
+          // x5gon Cookie Init should go here
           console.log('Required cookies allowed.');
-        }
-    
-        if (this.hasConsented('analytics')) {
-          console.log('Analytical cookies allowed.');
-        }
-    
-        if (this.hasConsented('marketing')) {
-          console.log('Marketing cookies allowed.');
         }
       },
     
       onAllow: function(category) 
       {
         if (category == 'required') {
+          // x5gon Cookie Init should go here
           console.log('Required cookies just allowed.');
-        }
-    
-        if (category == 'analytics') {
-          console.log('Analytical cookies just allowed.');
-        }
-    
-        if (category == 'marketing') {
-          console.log('Marketing cookies just allowed.');
         }
       },
     
       onRevoke: function(category) 
       {
         if (category == 'required') {
+          // x5gon Cookie destroy should go here
           console.log('Required cookies just revoked.');
-        }
-    
-        if (category == 'analytics') {
-          console.log('Analytical cookies just revoked.');
-        }
-    
-        if (category == 'marketing') {
-          console.log('Marketing cookies just revoked.');
         }
       }
     })

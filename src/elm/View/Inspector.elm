4c56d5a6765484b41dca271de7cfc2bab56235a1
Stefan Kreitmayer <stefankreitmayer@gmail.com>
2020-01-21 12:52:04 +0000

    hide text about range in inspector

--- src/elm/View/Inspector.elm
+++ src/elm/View/Inspector.elm
@@ -193,25 +193,24 @@ viewCourseSettings : Model -> Oer -> CourseItem -> List (Element Msg)
 viewCourseSettings model oer {range, comment} =
   let
       topRow =
-        let
-            rangeText =
-              if (range |> Debug.log "range") == Range 0 1 then
-                ""
-                else
-                "(Range "
-        in
-            [ "This video has been added to your workspace." ++ rangeText |> bodyWrap [ width fill ]
+        -- let
+        --     rangeText =
+        --       if (range |> Debug.log "range") == Range 0 1 then
+        --         ""
+        --       else
+        --         [ "(Range "
+        --         , range.start |> floor |> secondsToString
+        --         , " - "
+        --         , range.start + range.length |> floor |> secondsToString
+        --         , ")"
+        --         ]
+        --         |> String.join ""
+        -- in
+            -- [ "This video has been added to your workspace." ++ rangeText |> bodyWrap [ width fill ]
+            [ "This video has been added to your workspace." |> bodyWrap [ width fill ]
             , actionButtonWithIcon [] IconLeft "delete" "Remove" <| Just <| RemovedOerFromCourse oer.id
             ]
             |> row [ width fill, borderTop 1, Border.color greyDivider, paddingTop 10 ]
-
-      fields =
-        [ "Selected Range:"
-        , range.start |> floor |> secondsToString |> bodyNoWrap [ width fill ]
-        , "-" |> bodyNoWrap [ width fill ]
-        , range.start + range.length |> floor |> secondsToString |> bodyNoWrap [ width fill ]
-        ]
-        |> row [ spacing 10 ]
 
       commentField =
         Input.text [ width fill, htmlId "TextInputFieldForCommentOnCourseItem", onEnter <| SubmittedCourseItemComment, Border.color x5color, Font.size 14, padding 3, moveDown 5 ] { onChange = ChangedCommentTextInCourseItem oer.id, text = comment, placeholder = Just ("Enter any notes or comments about this item" |> text |> Input.placeholder [ Font.size 14, moveDown 6 ]), label = Input.labelHidden "Comment on course item" }
@@ -223,7 +222,7 @@ viewCourseSettings model oer {range, comment} =
           none
   in
       [ topRow
-      , [ fields, changesSaved ] |> row [ width fill ]
+      , [ changesSaved ] |> row [ width fill ]
       , commentField
       ]
 

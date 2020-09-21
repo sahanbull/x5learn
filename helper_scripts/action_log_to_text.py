d474f15dfd560811874a1fec9bcbf8c36b083a00
FurNasH <shenalpussegoda@gmail.com>
2020-09-21 11:38:05 +0530

    action log to csv function added

--- helper_scripts/action_log_to_text.py
+++ helper_scripts/action_log_to_text.py
@@ -2,17 +2,22 @@ import os
 import sqlalchemy as db
 from datetime import datetime
 import time
+import csv
 
 def main(args):
     print("X5learn - Creating text file based on action log...")
 
-    db_objects = ge_db_objects(args["dbuser"], args["dbpass"], args["dbname"])
+    db_objects = get_db_objects(args["dbuser"], args["dbpass"], args["dbname"])
     actions = db_objects[0]
     users = db_objects[1]
     connection = db_objects[2]
 
     query = db.select([users.c.id, users.c.email]).where(users.c.email.like("p%"))
     user_data = connection.execute(query).fetchall()
+
+    # list to hold session logs
+    all_sessions_data = list()
+    all_sessions_data.append(["Pid", "Datetime", "Task", "Duration", "ContentFlow Bar State", "Forced Complete Event"])
 
     # user lookup to find out user by id
     user_dict = dict()
@@ -22,6 +27,9 @@ def main(args):
     count = 0
     # fetching data user wise
     for id in user_dict.keys():
+        if user_dict[id] == "p0":
+            continue
+        
         query = db.select([actions.columns.created_at, actions.columns.events, actions.columns.user_login_id]).where(actions.columns.user_login_id == id)
         action_data = connection.execute(query).fetchall()
 
@@ -43,6 +51,9 @@ def main(args):
         # holds last state of content flow
         last_content_flow_state = find_initial_content_flow_state(action_data)
 
+        # holds a single session
+        session_data = list()
+
         for action in action_data:
 
             for event in action.events:
@@ -60,12 +71,25 @@ def main(args):
                         write_string += " | Content Flow State - " + last_content_flow_state
                         write_string += "\n\n"
 
-                    write_string += datetime.utcfromtimestamp(int(event['clientTime'][:10])).strftime("%Y-%m-%d %H:%M:%S")
-                    #write_string += " User - " + user_dict[action.user_login_id]
+                        if len(session_data) > 0:
+                            session_data.append(time.strftime('%H:%M:%S', time.gmtime(durationInSeconds)))
+                            session_data.append(last_content_flow_state)
+                            session_data.append("Yes")
+                            all_sessions_data.append(session_data)
+                            session_data = list()
+
+                    temp_date_string = datetime.utcfromtimestamp(int(event['clientTime'][:10])).strftime("%Y-%m-%d %H:%M:%S")
+                    write_string += temp_date_string
+                    # write_string += " User - " + user_dict[action.user_login_id]
                     write_string += " Started Task - " + "".join(event['args'])
                     write_string += " | Content Flow State - " + last_content_flow_state
 
+                    session_data.append(user_dict[id])
+                    session_data.append(temp_date_string)
+                    session_data.append("".join(event['args']))
+
                     last_session = int(event['clientTime'][:10])
+                    
 
                 elif event['eventType'] == "CompleteTask":
                     durationInSeconds = 0
@@ -78,6 +102,13 @@ def main(args):
                     write_string += " Completed Task | Duration - " + time.strftime('%H:%M:%S', time.gmtime(durationInSeconds))
                     write_string += " | Content Flow State - " + last_content_flow_state
                     write_string += "\n"
+
+                    if len(session_data) > 0:
+                        session_data.append(time.strftime('%H:%M:%S', time.gmtime(durationInSeconds)))
+                        session_data.append(last_content_flow_state)
+                        session_data.append("No")
+                        all_sessions_data.append(session_data)
+                        session_data = list()
 
                 elif event['eventType'] == "ToggleContentFlow":
                     durationInSeconds = 0
@@ -107,6 +138,14 @@ def main(args):
 
         f.close()
 
+        # writing data to a csv file
+        if len(all_sessions_data) > 0:
+            with open(write_path + "processed-logs.csv", 'w', newline='') as file:
+                writer = csv.writer(file)
+
+                for session in all_sessions_data:
+                    writer.writerow(session)
+
     print("\nEnding script. No of events processed : ", count)
 
 
@@ -126,7 +165,13 @@ def find_initial_content_flow_state(action_data):
     return initial_state if initial_state is not None else "disabled"
     
 
-def ge_db_objects(user, passwd, db_name):
+def seconds_to_time_string(seconds):
+    return seconds
+    seconds = int(float(seconds))
+    duration = str(int(seconds / 60)) + ':' + str(seconds % 60).zfill(2)
+    return duration
+
+def get_db_objects(user, passwd, db_name):
     db_engine_uri = 'postgresql://{}:{}@localhost:5432/{}'.format(
         user, passwd, db_name)
     engine = db.create_engine(db_engine_uri)

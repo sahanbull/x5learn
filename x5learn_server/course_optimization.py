8e0fd3bd0dfe2e6c5d0e7fb46cfd1ea0a7b1f656
FurNasH <shenalpussegoda@gmail.com>
2020-10-22 09:31:47 +0530

    wp3 api url fixed

--- x5learn_server/course_optimization.py
+++ x5learn_server/course_optimization.py
@@ -17,7 +17,7 @@ def convert_to_oer_ids(material_ids):
 
 def optimize_course(oer_ids):
     # API base url
-    API_URL = "https://wp3dev.x5gon.org"
+    API_URL = "https://wp3.x5gon.org"
 
     # setup appropriate headers
     HEADERS = {

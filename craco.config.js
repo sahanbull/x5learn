7089f8365245c0d808a5ac8c6316f063eab93dfa
ruhaim <ruhaim@gardenconnect.com>
2020-10-23 19:08:38 +0530

    Add craco to alter build output structure of the react app

---
+++ craco.config.js
@@ -0,0 +1,39 @@
+const {
+  when,
+  whenDev,
+  whenProd,
+  whenTest,
+  ESLINT_MODES,
+  POSTCSS_MODES,
+} = require('@craco/craco');
+const path = require('path');
+
+module.exports = {
+  webpack: {
+    alias: {},
+    plugins: [],
+    configure: {
+      /* Any webpack configuration options: https://webpack.js.org/configuration */
+    },
+    configure: (webpackConfig, { env, paths }) => {
+      //html template
+      //   webpackConfig.plugins[0].options.template = path.resolve(
+      //     'public/home.html',
+      //   );
+      webpackConfig.plugins[0].options.filename = 'templates/home.html';
+
+      //Css
+      webpackConfig.plugins[5].options.chunkFilename =
+        '' + webpackConfig.plugins[5].options.chunkFilename;
+      webpackConfig.plugins[5].options.filename =
+        '' + webpackConfig.plugins[5].options.filename; // 'static/css/[name].[contenthash:8].css'
+      //Manifest
+
+      webpackConfig.output.chunkFilename =
+        '' + webpackConfig.output.chunkFilename;
+      webpackConfig.output.filename = '' + webpackConfig.output.filename;
+      debugger;
+      return webpackConfig;
+    },
+  },
+};

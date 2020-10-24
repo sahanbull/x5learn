a95de38c12349794e75adb3840ac5921a63ec6e4
ruhaim <ruhaim@gardenconnect.com>
2020-10-24 22:55:54 +0530

    alter package.json and init.sh for deployment, use yarn build:craco or sh init.sh

--- init.sh
+++ init.sh
@@ -4,8 +4,12 @@ PYTHON_PATH="/home/ucl/anaconda3/envs/x5learn/bin/"
 
 $PYTHON_PATH/pip install -e .
 
-npm run setup
-npm run build
+yarn setup
+#npm run build
+
+yarn build:craco
+yarn deploy:craco
+
 
 cp -r uncompressed/* x5learn_server/static/dist
 cp -r assets/img x5learn_server/static/dist/img

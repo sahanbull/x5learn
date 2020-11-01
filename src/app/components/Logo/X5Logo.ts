23300bf4b16b8312392970481483a8e7f2ae88a1
ruhaim <ruhaim@gardenconnect.com>
2020-11-01 20:07:35 +0530

    setup embeding logos via JS

--- src/app/components/Logo/X5Logo.ts
+++ src/app/components/Logo/X5Logo.ts
@@ -1,4 +1,6 @@
 import styled, { css } from 'styled-components';
+// import logo from './img/x5learn_logo_new.png';
+// import white_logo from './img/x5learn_logo_new_white.png';
 
 export const X5Logo = styled.div<{ white?: boolean }>`
   background-image: url('/static/img/x5learn_logo_new.png');

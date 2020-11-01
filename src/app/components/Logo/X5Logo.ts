0e1ec599e0cd10fd8aed8c0596e5722343177a6a
ruhaim <ruhaim@gmail.com>
2020-11-01 22:47:00 +0530

    Align header logo to center

--- src/app/components/Logo/X5Logo.ts
+++ src/app/components/Logo/X5Logo.ts
@@ -4,10 +4,8 @@ import styled, { css } from 'styled-components';
 
 export const X5Logo = styled.div<{ white?: boolean }>`
   background-image: url('/static/img/x5learn_logo_new.png');
-  float: left;
   width: 120px;
-  height: 31px;
-  margin: 16px 24px 16px 0;
+  height: 24px;
   background-repeat: no-repeat;
   background-size: contain;
   ${props =>

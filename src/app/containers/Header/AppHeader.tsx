324c3b34b8a4dea977498f8e2cbfec1da0adcb36
ruhaim <ruhaim@gmail.com>
2020-11-02 11:37:45 +0530

    add searchbar and profile widget to header

--- src/app/containers/Header/AppHeader.tsx
+++ src/app/containers/Header/AppHeader.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
-import { Layout, Menu, Breadcrumb, Space } from 'antd';
+import {
+  Layout,
+  Menu,
+  Breadcrumb,
+  Space,
+  Button,
+  Tag,
+  Typography,
+  Row,
+  PageHeader,
+  Col,
+} from 'antd';
 import {
   UserOutlined,
   LaptopOutlined,
@@ -8,6 +19,7 @@ import {
 import { X5Logo } from 'app/components/Logo/X5Logo';
 import styled from 'styled-components';
 import { HeaderSearchBar } from './HeaderSearchBar';
+import { HeaderProfileWidget } from '../../components/HeaderProfileWidget/HeaderProfileWidget';
 
 const { SubMenu } = Menu;
 const { Header, Content, Sider } = Layout;
@@ -15,20 +27,25 @@ const { Header, Content, Sider } = Layout;
 const StyledHeader = styled(Header)`
   box-shadow: 0 2px 8px 0 rgba(0, 19, 77, 0.16);
   z-index: 50;
-  display: flex;
-  align-items: center;
+  /* display: flex;
+  align-items: center; */
 `;
 
 export function AppHeader(props) {
   return (
     <StyledHeader className="header">
-      <X5Logo />
-      <HeaderSearchBar />
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-        <Menu.Item key="1">nav 1</Menu.Item>
-        <Menu.Item key="2">nav 2</Menu.Item>
-        <Menu.Item key="3">nav 3</Menu.Item>
-      </Menu>
+      <Row align="middle" justify="space-between">
+        <Col span={6}>
+          <X5Logo />
+        </Col>
+        <Col span={12}>
+          <HeaderSearchBar />
+        </Col>
+        <Col span={6} style={{ textAlign: 'right' }}>
+          <Button type="link">About</Button>
+          <HeaderProfileWidget />
+        </Col>
+      </Row>
     </StyledHeader>
   );
 }

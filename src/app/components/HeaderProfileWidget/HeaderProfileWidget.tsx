4d4edf1d97ce22c92d90935e72c7e652f31d7a89
ruhaim <ruhaim@gmail.com>
2020-11-02 11:36:37 +0530

    app header profile widget

---
+++ src/app/components/HeaderProfileWidget/HeaderProfileWidget.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import {
+  Layout,
+  Menu,
+  Breadcrumb,
+  Input,
+  Space,
+  Button,
+  Row,
+  Col,
+  Form,
+  Popover,
+  Image,
+  Typography,
+} from 'antd';
+import {
+  UserOutlined,
+  LaptopOutlined,
+  AppstoreOutlined,
+} from '@ant-design/icons';
+
+import styled from 'styled-components';
+import Column from 'antd/lib/table/Column';
+
+const { Title, Link, Text } = Typography;
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
+const { Search } = Input;
+
+export function HeaderProfileWidget(props) {
+  return (
+    <>
+      <Popover
+        placement="bottomLeft"
+        title={
+          <>
+            <Text strong>Name Surname</Text>
+            <br />
+            <Text>name.surname@email.si</Text>
+          </>
+        }
+        content={
+          <>
+            <Link href="#">My Profile</Link>
+            <br />
+            <Link href="#">Logout</Link>
+          </>
+        }
+        trigger="click"
+      >
+        <Button
+          size="large"
+          icon={
+            <Image preview={false} width={40} src="/static/favicon.ico"></Image>
+          }
+        />
+      </Popover>
+    </>
+  );
+}

3dcddf3cdd5ced1a363161a5452882e08864e95b
Dhanushka Bandara <dhanushka.bandara@outlook.com>
2022-12-15 11:18:01 +0530

    Add unveilAi

--- src/app/components/EnrichmentBar/EntityDefinitionMenuItem.tsx
+++ src/app/components/EnrichmentBar/EntityDefinitionMenuItem.tsx
@@ -1,19 +1,46 @@
-import { Menu, Dropdown, Spin } from 'antd';
+import { Menu, Spin, Popover } from 'antd';
 import { selectEntityDefinition } from 'app/containers/Layout/ducks/allEntityDefinitionsSlice';
 import { useTranslation } from 'react-i18next';
 
-import { useDispatch, useSelector } from 'react-redux';
-import { Link, useHistory, useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { ROUTES } from 'routes/routes';
+import { selectUnveilAi } from 'app/containers/Header/HeaderSlice';
+import { AppstoreOutlined } from '@ant-design/icons';
 
 export const EntityDefinitionMenuItem = ({ entity, ...props }) => {
+  const unveilAi = useSelector(selectUnveilAi);
   const { data, loading, error } = useSelector(state => {
     return selectEntityDefinition(state, entity.id);
   });
-  const history = useHistory();
   const { t } = useTranslation();
   return (
-    <>
+    <div
+      style={{
+        border: unveilAi ? '2px solid red' : 'none',
+        width: 'fit-content',
+      }}
+    >
+      {unveilAi && (
+        <Popover
+          title=""
+          content={
+            <div style={{ width: '175px' }}>
+              <p>
+                The JSI Wikifier is a web service that takes a text document as
+                input and annotates it with links to relevant Wikipedia
+                concepts.
+              </p>
+              <a href="#">Try it yourself</a>
+            </div>
+          }
+          trigger="hover"
+        >
+          <p style={{ textAlign: 'center' }}>
+            <AppstoreOutlined />
+          </p>
+        </Popover>
+      )}
       {error && <>{error}</>}
       {loading && <Spin spinning={loading} delay={500}></Spin>}
       {data && (
@@ -43,6 +70,6 @@ export const EntityDefinitionMenuItem = ({ entity, ...props }) => {
           {t('generic.lbl_wikipedia')}
         </a>
       </Menu.Item>
-    </>
+    </div>
   );
 };
--- src/app/pages/HomePage/components/FeaturedOER/RecomendedOerList.tsx
+++ src/app/pages/HomePage/components/FeaturedOER/RecomendedOerList.tsx
@@ -3,14 +3,16 @@ import { OerCardList } from './OerCardList';
 import { useTranslation } from 'react-i18next';
 import { fetchFeaturedOer, sliceKey } from '../../ducks/featuredOerSlice';
 
-import { Col, Row, Typography } from 'antd';
+import { Col, Row, Typography, Popover } from 'antd';
 import { useSelector } from 'react-redux';
 import { OerCard } from './OerCard';
+import { selectUnveilAi } from 'app/containers/Header/HeaderSlice';
 
 const { Title } = Typography;
 
 export function RecomendedOerList() {
   const { t } = useTranslation();
+  const unveilAi = useSelector(selectUnveilAi);
 
   const loading = useSelector(state => {
     return state[sliceKey].loading;
@@ -24,7 +26,34 @@ export function RecomendedOerList() {
 
   return (
     <div>
-      <Title level={4}>{t('oer.recommended', 'Recommended for You')}</Title>
+      {unveilAi ? (
+        <Popover
+          title=""
+          content={
+            <div style={{ maxWidth: '400px' }}>
+              <p>
+                By joining forces and connecting your OER site to all existing
+                OER sites into the first Global OER Network, we can together
+                unleash the equity potential of OER and start the first data
+                driven effort capable of understanding and recommending OERs
+                across different sites,languages, modalities such as video,
+                documents and textbooks.
+              </p>
+              <a href="#">Try it yourself</a>
+            </div>
+          }
+          trigger="hover"
+        >
+          <Title
+            style={{ border: '2px solid red', width: 'fit-content' }}
+            level={4}
+          >
+            {t('oer.recommended', 'Recommended for You')}
+          </Title>
+        </Popover>
+      ) : (
+        <Title level={4}>{t('oer.recommended', 'Recommended for You')}</Title>
+      )}
       <OerCardList loading={loading} error={error} data={featuredData} />
     </div>
   );

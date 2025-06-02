8bf5ecc4f8612df3eb24bd01f3ba855bb7eecd54
Shenal Pussegoda <shenalpussegoda@gmail.com>
2025-06-02 11:24:40 +0530

    fix: ensure OER data is updated correctly before saving to the repository

--- x5learn_server/app.py
+++ x5learn_server/app.py
@@ -2520,6 +2520,8 @@ class Temp_Playlist_Youtube_Item_Update(Resource):
                 oer_data['description'] = api.payload.get('description', oer_data['description'])
 
                 # to enfore the data change let's json dump it
+                oer.data = json.dumps(oer_data)
+                repository.update()
                 oer.data = json.loads(json.dumps(oer_data))
 
                 # let's update the oer in the database
